7e2c85bf87f8612bac0bf0806a7dd087185ce179
Mark Deanil Vicente <markdeanilvicente@outlook.com>
2018-04-13 19:11:53 +0800

    Add project files.

---
+++ ClientApp/components/fruitinventory/fruit.ts
@@ -0,0 +1,24 @@
+﻿import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component
+
+export default class Fruit extends Vue {
+    fruitName: string = "Apple";
+
+    fruits = [{
+            id: 0,
+            name: 'Mango',
+            price: 100
+        },
+        {
+            id: 1,
+            name: 'Apple',
+            price: 200
+        },
+        {
+            id: 2,
+            name: 'Orange',
+            price: 300
+        }];
+}
